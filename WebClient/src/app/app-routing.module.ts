752667d53ac74993e935e75e96db12f192ce6204
doreen <doreen.madkour85@gmail.com>
2022-07-21 15:02:58 +0200

    add about-us/news/contact-us components to build the header menu

--- WebClient/src/app/app-routing.module.ts
+++ WebClient/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { NewsComponent } from './news/news.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomePageComponent }
+  { path: 'home', component: HomePageComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'news', component: NewsComponent },
+  { path: 'contact-us', component: ContactUsComponent }
 ];
 
 @NgModule({
--- WebClient/src/app/app.module.ts
+++ WebClient/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { NewsComponent } from './news/news.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent,
+    NewsComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
