01e43778bb7e3d3cacd23ced2f4c03d3e7913f00
doreen <doreen.madkour85@gmail.com>
2022-07-23 01:33:47 +0200

    t11 - add things to do component

--- WebClient/src/app/home-page/home-page.module.ts
+++ WebClient/src/app/home-page/home-page.module.ts
@@ -5,13 +5,15 @@ import { CoreModule } from '../core/core.module';
 import { LatestNewsComponent } from './latest-news/latest-news.component';
 import { SharedModule } from '../shared/shared.module';
 import { HighlightsBannerComponent } from './highlights-banner/highlights-banner.component';
+import { ThingsWeDoComponent } from './things-we-do/things-we-do.component';
 
 
 @NgModule({
   declarations: [
     HomePageComponent,
     LatestNewsComponent,
-    HighlightsBannerComponent
+    HighlightsBannerComponent,
+    ThingsWeDoComponent
   ],
   imports: [
     CommonModule,
