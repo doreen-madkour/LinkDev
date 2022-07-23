6205d62f37ec8154d6c4b4e70ead539f5a893a48
doreen <doreen.madkour85@gmail.com>
2022-07-23 15:48:24 +0200

    t11- t12 style right and left columns in things to do section

--- WebClient/src/app/home-page/things-we-do/things-we-do.component.ts
+++ WebClient/src/app/home-page/things-we-do/things-we-do.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./things-we-do.component.scss']
 })
 export class ThingsWeDoComponent implements OnInit {
+  services!:any[];
+  servicesImgBaseUrl: string = "../../../assets/images/home-page/services/";
 
   constructor() { }
 
   ngOnInit(): void {
+    this.services = [
+      { name: 'transformation' , order: 1, imgUrl: `${this.servicesImgBaseUrl + 'transformation.svg'}`},
+      { name: 'envision' ,  order: 2, imgUrl:  `${this.servicesImgBaseUrl + 'enovision.svg'}`},
+      { name: 'dynamics 365' ,  order: 2, imgUrl:  `${this.servicesImgBaseUrl + 'dynamics.svg'}`},
+      { name: 'crafty mind' ,  order: 3, imgUrl:  `${this.servicesImgBaseUrl + 'crafty.svg'}`},
+      { name: 'services' ,  order: 3, imgUrl:  `${this.servicesImgBaseUrl + 'services.svg'}`}
+    ]
   }
 
 }
